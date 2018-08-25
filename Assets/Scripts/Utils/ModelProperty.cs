a30d0d713b7e0eb56610c72fe9a0c003460c338a
polvila <polvilasalo@gmail.com>
2018-08-25 12:05:59 +0200

    Add a generic ModelProperty

---
+++ Assets/Scripts/Utils/ModelProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ModelProperty<T>
+{
+    private T _property;
+    public T Property
+    {
+        get { return _property; }
+        set
+        {
+            if (Compare(_property,value)) return;
+            _property = value;
+            PropertyChanged?.Invoke(_property);
+        }
+    }
+    
+    public event Action<T> PropertyChanged;
+    
+    private bool Compare(T x, T y)
+    {
+        return EqualityComparer<T>.Default.Equals(x, y);
+    }
+}
